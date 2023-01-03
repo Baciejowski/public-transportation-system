6cea901d9a2547d7a77666eb3e7913dbd2df8311
kwojcikowski <wojcikowskikacper5@gmail.com>
2023-01-03 18:35:09 +0100

    Added discovery service

---
+++ backend/discovery/src/test/kotlin/pl/edu/pwr/psi/epk/discovery/DiscoveryApplicationTests.kt
@@ -0,0 +1,13 @@
+package pl.edu.pwr.psi.epk.discovery
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DiscoveryApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
