3c159d778382f7ee29061333df92a951e5813bd7
kwojcikowski <wojcikowskikacper5@gmail.com>
2023-01-03 18:28:31 +0100

    Added gateway service

---
+++ backend/gateway/src/main/kotlin/pl/edu/pwr/psi/epk/gateway/GatewayApplication.kt
@@ -0,0 +1,11 @@
+package pl.edu.pwr.psi.epk.gateway
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GatewayApplication
+
+fun main(args: Array<String>) {
+    runApplication<GatewayApplication>(*args)
+}
