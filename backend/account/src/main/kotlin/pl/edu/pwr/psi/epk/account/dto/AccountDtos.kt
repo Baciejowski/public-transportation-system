e36d61004817a8bae8748d714379309332de6dab
kwojcikowski <wojcikowskikacper5@gmail.com>
2023-01-28 14:45:02 +0100

    Applied review suggestions

--- backend/account/src/main/kotlin/pl/edu/pwr/psi/epk/account/dto/AccountDtos.kt
+++ backend/account/src/main/kotlin/pl/edu/pwr/psi/epk/account/dto/AccountDtos.kt
@@ -1,30 +1,44 @@
 package pl.edu.pwr.psi.epk.account.dto
 
-import com.fasterxml.jackson.annotation.JsonInclude
 import pl.edu.pwr.psi.epk.account.model.Passenger
 import pl.edu.pwr.psi.epk.account.model.Role
 import pl.edu.pwr.psi.epk.account.model.User
 
-data class UserReadDto(
+open class UserReadDto(
     val id: Long,
     val email: String,
     val firstName: String,
     val lastName: String,
     val role: Role,
-
-    @JsonInclude(JsonInclude.Include.NON_NULL)
-    val accountBalance: Double?
 ) {
 
     companion object {
-        fun fromUser(user: User): UserReadDto = UserReadDto(
-            user.id,
-            user.email,
-            user.firstName,
-            user.lastName,
-            user.role,
-            kotlin.runCatching { (user as Passenger).walletBalance }.getOrNull()
-        )
-
+        fun fromUser(user: User): UserReadDto =
+            when (user) {
+                is Passenger -> PassengerReadDto(
+                    user.id,
+                    user.email,
+                    user.firstName,
+                    user.lastName,
+                    user.role,
+                    user.walletBalance
+                )
+                else -> UserReadDto(
+                    user.id,
+                    user.email,
+                    user.firstName,
+                    user.lastName,
+                    user.role
+                )
+            }
     }
 }
+
+class PassengerReadDto(
+    id: Long,
+    email: String,
+    firstName: String,
+    lastName: String,
+    role: Role,
+    val accountBalance: Double
+): UserReadDto(id, email, firstName, lastName, role)
