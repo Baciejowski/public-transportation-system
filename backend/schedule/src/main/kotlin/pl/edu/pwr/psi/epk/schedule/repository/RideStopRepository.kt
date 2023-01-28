6a950ac80c07105d8be76405a6fe439c09669382
Baciejowski <k.baciejowski@gmail.com>
2023-01-28 18:43:14 +0100

    Add repositories

---
+++ backend/schedule/src/main/kotlin/pl/edu/pwr/psi/epk/schedule/repository/RideStopRepository.kt
@@ -0,0 +1,6 @@
+package pl.edu.pwr.psi.epk.schedule.repository
+
+import org.springframework.data.jpa.repository.JpaRepository
+import pl.edu.pwr.psi.epk.schedule.model.RideStop
+
+interface RideStopRepository: JpaRepository<RideStop, Long>
