07b157fa73e39ce6d5691c90c531a4cfda6d8e3e
Baciejowski <k.baciejowski@gmail.com>
2023-01-29 18:47:12 +0100

    Add method to find routeStops by stop id

--- backend/schedule/src/main/kotlin/pl/edu/pwr/psi/epk/schedule/repository/RouteServiceStopRepository.kt
+++ backend/schedule/src/main/kotlin/pl/edu/pwr/psi/epk/schedule/repository/RouteServiceStopRepository.kt
@@ -3,4 +3,6 @@ package pl.edu.pwr.psi.epk.schedule.repository
 import org.springframework.data.jpa.repository.JpaRepository
 import pl.edu.pwr.psi.epk.schedule.model.RouteServiceStop
 
-interface RouteServiceStopRepository: JpaRepository<RouteServiceStop, Long>
+interface RouteServiceStopRepository: JpaRepository<RouteServiceStop, Long> {
+    fun findAllByStop_Id(stopId: Long): List<RouteServiceStop>
+}
