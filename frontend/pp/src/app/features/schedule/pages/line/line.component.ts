36930fd797ece890e507229667705f8c931d9021
Dasarro <wicia0202@wp.pl>
2023-01-31 00:36:24 +0100

    Add unsaved file

--- frontend/pp/src/app/features/schedule/pages/line/line.component.ts
+++ frontend/pp/src/app/features/schedule/pages/line/line.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-line',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LineComponent implements OnInit {
 
-  constructor() { }
+  lineId: string;
+
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.lineId =  this.route.snapshot.paramMap.get('lineId')!;
   }
 
 }
