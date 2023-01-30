aee6e3c96c182d0d66919ae8bdfc99d2fc756537
Baciejowski <k.baciejowski@gmail.com>
2023-01-30 21:18:30 +0100

    Fix unresolved reference: findAllByStartTimeIsNotNullAndEndTimeIsNull

--- backend/schedule/src/main/kotlin/pl/edu/pwr/psi/epk/schedule/repository/RideRepository.kt
+++ backend/schedule/src/main/kotlin/pl/edu/pwr/psi/epk/schedule/repository/RideRepository.kt
@@ -3,4 +3,6 @@ package pl.edu.pwr.psi.epk.schedule.repository
 import org.springframework.data.jpa.repository.JpaRepository
 import pl.edu.pwr.psi.epk.schedule.model.Ride
 
-interface RideRepository: JpaRepository<Ride, Long>
+interface RideRepository: JpaRepository<Ride, Long> {
+    fun findAllByStartTimeIsNotNullAndEndTimeIsNull(): List<Ride>
+}
