b0e33a7a3ab407849f63ecb128a51b5b14432ad9
Dasarro <wicia0202@wp.pl>
2023-01-31 17:03:21 +0100

    Finish prototype of line view

--- frontend/pp/src/app/features/schedule/pages/line/line.component.ts
+++ frontend/pp/src/app/features/schedule/pages/line/line.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { Observable } from 'rxjs';
+import { Observable, Subject, takeUntil, tap } from 'rxjs';
 import { RouteDetailDto } from '../../models/routeDetailDto';
 import { RouteManifestDto } from '../../models/routeManifestDto';
 import { ScheduleService } from '../../services/schedule.service';
@@ -10,21 +10,28 @@ import { ScheduleService } from '../../services/schedule.service';
   templateUrl: './line.component.html',
   styleUrls: ['./line.component.scss']
 })
-export class LineComponent implements OnInit {
+export class LineComponent implements OnInit, OnDestroy {
 
   lineRoutes$: Observable<RouteManifestDto[]>;
-  route$: Observable<RouteDetailDto>;
+  routeDetails: RouteDetailDto;
+  destroy$ = new Subject();
 
   constructor(private route: ActivatedRoute, private scheduleService: ScheduleService) { }
-
+  
   ngOnInit(): void {
     const lineId = this.route.snapshot.paramMap.get('lineId')!;
     this.lineRoutes$ = this.scheduleService.getLineRoutes(lineId);
   }
-
+  
   switchRoute(routeId: string) {
     console.log(routeId)
-    this.route$ = this.scheduleService.getRouteDetails(routeId);
+    this.scheduleService.getRouteDetails(routeId)
+      .pipe(takeUntil(this.destroy$))
+      .subscribe(routeDetails => this.routeDetails = routeDetails);
   }
 
+  ngOnDestroy(): void {
+    this.destroy$.next(true);
+    this.destroy$.complete();
+  }
 }
